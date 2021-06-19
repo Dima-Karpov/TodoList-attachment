c2ad333975a0140f9100f20dfdb524127ad2d90a
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-06-19 11:25:05 +0300

    'storybook-AppWithRedux'

--- src/stories/storiesTo/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/storiesTo/decorators/ReduxStoreProviderDecorator.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../../state/store';
 
-export const ReduxStoreProviderDecorator = (storyFn: any) => {
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
     return <Provider store={store}>{storyFn()}</Provider>
-}
+};
