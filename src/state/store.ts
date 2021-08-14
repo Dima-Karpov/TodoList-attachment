45c882b23f49a205af095260edab3ce3d80a750c
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-08-14 21:38:42 +0300

    'auth'

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,11 +3,13 @@ import { tasksReducer } from "./task-reducer";
 import { todoListReduser } from "./TodoList-reducer";
 import thunk from 'redux-thunk';
 import { appReducer } from "./app-reducer";
+import { authReducer } from "../components/features/Login/auth-reducer";
 
 const rootReduser = combineReducers({
     tasks: tasksReducer,
     todoLists: todoListReduser,
     app: appReducer,
+    auth: authReducer,
 });
 
 // type AppRootStateType = {
