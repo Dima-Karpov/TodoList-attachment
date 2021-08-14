afbf79a79f7e8e2513c2f3152f3d1334b6d96054
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-08-14 20:27:13 +0300

    'login-switch-router'

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -8,14 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Container, Grid, Paper } from '@material-ui/core';
-import {
-    addTodolistsTC, changeTodoListFilterAC, changeTodoListTitleTC,
-    fetchTodoListsTC, TodoListDomainType, removeTodolistsTC,
-    FilterValuesType
-} from '../state/TodoList-reducer';
-import {
-    removeTaskTC, updateTaskTC, addTaskTC, TaskStateType } from '../state/task-reducer';
+import { Container } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppRootStateType } from '../state/store';
 import { TaskStatuses, TaskType } from '../api/todolist-api';
@@ -23,6 +16,8 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import { RequestStatusType } from '../state/app-reducer';
 import { ErrorSnacbar } from '../components/ErrorSnackbar/ErrorSnackbar';
 import { TodolistsList } from '../components/features/Todolist/TodolistsList';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { Login } from './../components/features/Login/Login';
 
 
 
@@ -31,23 +26,32 @@ export function AppWithRedux() {
     const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status);
 
     return (
-        <div className="App">
-            <AppBar position="static">
-                <Toolbar style={{ justifyContent: 'space-between' }}>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6">
-                        TodoList
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-                {status === 'loading' && <LinearProgress />}
-            </AppBar>
-            <Container fixed>
-                <TodolistsList />
-            </Container>
-            <ErrorSnacbar/>
-        </div> 
+        <BrowserRouter>
+            <div className="App">
+                <AppBar position="static">
+                    <Toolbar style={{ justifyContent: 'space-between' }}>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6">
+                            TodoList
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                    {status === 'loading' && <LinearProgress />}
+                </AppBar>
+                <Container fixed>
+
+                    <Switch>
+                        <Route exact path={'/'} render={() => <TodolistsList />} />
+                        <Route path={'/login'} render={() => <Login />} />
+                        <Route path={'/404'} render={() => <h1 style={{ fontSize: '50px', textAling: 'center' }}>404: PAGE NOT FOUND</h1>} />
+                        <Redirect from={'*'} to={'/404'} />
+                    </Switch>
+
+                </Container>
+                <ErrorSnacbar />
+            </div>
+        </BrowserRouter>
     );
 }
