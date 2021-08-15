6da04f5409cdea8fbab307978407568f0a2c08ea
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-08-15 06:05:56 +0300

    'refactor-bag-realizeichion-loading''

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { Container } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppRootStateType } from '../state/store';
@@ -15,7 +16,7 @@ import { ErrorSnacbar } from '../components/ErrorSnackbar/ErrorSnackbar';
 import { TodolistsList } from '../components/features/Todolist/TodolistsList';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { Login } from './../components/features/Login/Login';
-import {initializeAppTC} from '../state/app-reducer';
+import { initializeAppTC } from '../state/app-reducer';
 
 
 
@@ -23,10 +24,18 @@ export function AppWithRedux() {
 
     const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status);
     const dispatch = useDispatch();
+    const isInitialized = useSelector<AppRootStateType, boolean>(state => state.app.isInitialized);
 
     useEffect(() => {
         dispatch(initializeAppTC())
-    }, [dispatch])
+    }, [dispatch]);
+
+    if (!isInitialized) {
+        return <div
+            style={{ position: 'fixed', top: '30%', textAlign: 'center', width: '100%' }}>
+            <CircularProgress />
+        </div>
+    };
 
     return (
         <BrowserRouter>
@@ -48,7 +57,7 @@ export function AppWithRedux() {
                     <Switch>
                         <Route exact path={'/'} render={() => <TodolistsList />} />
                         <Route path={'/login'} render={() => <Login />} />
-                        <Route path={'/404'} render={() => <h1 style={{fontSize: '50px', textAlign: 'center'}}>404: PAGE NOT FOUND</h1>} />
+                        <Route path={'/404'} render={() => <h1 style={{ fontSize: '50px', textAlign: 'center' }}>404: PAGE NOT FOUND</h1>} />
                         <Redirect from={'*'} to={'/404'} />
                     </Switch>
 
